d968e6d4c3c709168466ffb32c182fa5e770c60d
BruDevin <bruno.ads.2022@gmail.com>
2023-11-29 15:06:49 -0300

    Spring accessing MongoDB Data with REST

---
+++ src/main/java/com/example/springmongodbrest/SpringmongodbrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmongodbrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongodbrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmongodbrestApplication.class, args);
+	}
+
+}
